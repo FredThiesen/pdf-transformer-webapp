d8889aea9dd3ad8db8eae18dc5bcbb46739eccdb
Ricardo Frederico Thiesen <ricardothiesen@outlook.com>
2025-08-04 08:32:39 -0300

    fix: add padding to MaxRowsInput component for improved layout

--- src/components/MaxRowsInput.tsx
+++ src/components/MaxRowsInput.tsx
@@ -13,7 +13,7 @@ const MaxRowsInput: React.FC<MaxRowsInputProps> = ({ value, onChange }) => {
 	}
 
 	return (
-		<div className="flex flex-col gap-2">
+		<div className="flex flex-col gap-2 p-2">
 			<label htmlFor="maxRows" className="text-sm text-white">
 				Número máximo de linhas por página A4 (opcional):
 			</label>
@@ -24,8 +24,7 @@ const MaxRowsInput: React.FC<MaxRowsInputProps> = ({ value, onChange }) => {
 				placeholder="Deixe em branco para usar o máximo de linhas possível"
 				value={value || ""}
 				onChange={handleChange}
-				// O valor agora representa o número de linhas por página A4
-				className="block w-full max-w-sm text-sm text-white bg-primary border border-tertiary rounded-lg cursor-text focus:outline-none focus:ring-2 focus:ring-tertiary  transition-all"
+				className="block w-full max-w-sm text-lg text-white bg-primary border border-tertiary rounded-lg cursor-text focus:outline-none focus:ring-2 focus:ring-tertiary transition-all p-2"
 			/>
 		</div>
 	)
